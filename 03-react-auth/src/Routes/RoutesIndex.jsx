91ca023dfe25bbbaa8d7c79f3db36efae00c5d8e
Villagran <luisbmx_haro@hotmail.com>
2024-08-08 16:32:53 -0600

    Arreglo problemas con path

--- 03-react-auth/src/Routes/RoutesIndex.jsx
+++ 03-react-auth/src/Routes/RoutesIndex.jsx
@@ -2,12 +2,13 @@
 import { Routes, Route } from 'react-router-dom'
 // Importante tambien importar BrowserRouter
 // y RoutesIndex en App.jsx
-import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
+import { Home, Dashboard, Login, Secret, Signup } from '../Pages'
 
 const RoutesIndex = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/login' element={<Login />} />
       <Route path='/secret' element={<Secret />} />
