3a928d640c76fdee76df50f2036eec6d7972c0fc
Villagran <luisbmx_haro@hotmail.com>
2024-08-08 19:48:04 -0600

    Arreglo problemas con path, se agrega linkIsActive a clclassName y se da configura correctamente @

--- 03-react-auth/src/Components/Header/Header.jsx
+++ 03-react-auth/src/Components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import { NavLink } from 'react-router-dom'
-import './header.scss'
+import '@/Components/Header/header.scss'
 
 const Header = () => {
+    const linkIsActive = (isActive) => isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
+
     return (
         <>
             {/* BEM PARA NUESTRAS CLASES EN HTML */}
             {/* kebabcase para nombrar nuestras className
         en React con minúsculas */}
             <nav className="header">
-                {" "}
                 {/*  BLOQUE */}
                 <NavLink className="header__logo" to="/">
                     LOGO
@@ -16,29 +17,29 @@ const Header = () => {
                 <ul className="header__nav-list" />
                 <li className="header__list-item">
                     <NavLink
-                        className="header__item-link header__item-link--is-active"
+                        className={({ isActive }) => linkIsActive(isActive)}
                         to="/home"
                     >
                         Home
                     </NavLink>
                 </li>
                 <li className="header__list-item">
-                    <NavLink className="header__item-link" to="/dashboard">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/dashboard">
                         Dashboard
                     </NavLink>
                 </li>
                 <li className="header__list-item">
-                    <NavLink className="header__item-link" to="/secret">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/secret">
                         Secret
                     </NavLink>
                 </li>
                 <li className="header__list-item">
-                    <NavLink className="header__item-link" to="/login">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/login">
                         Login
                     </NavLink>
                 </li>
                 <li className="header__list-item">
-                    <NavLink className="header__item-link" to="/signup">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/signup">
                         Signup
                     </NavLink>
                 </li>
