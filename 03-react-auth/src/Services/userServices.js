ccd39964e746de0f644724e16cc757572444e8b9
Villagran <luisbmx_haro@hotmail.com>
2024-08-11 21:09:06 -0600

    Llamado a la API

---
+++ 03-react-auth/src/Services/userServices.js
@@ -0,0 +1,20 @@
+// Llamada a API
+// Pasamos endepoint para traer data
+
+// 1.- Importar axios
+import axios from 'axios'
+
+// 2.- Indicar ruta o endpoint
+const BASE_URL = 'http://localhost:3000' // Esto se puede cambiar si subimos el repo de git al render
+
+// 3.- Necesitamos mandar información
+// Registrar un usuario
+const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
+// Login
+const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+// Información de un solo usuario
+
+export {
+    registerUserService,
+    loginUserService
+}
