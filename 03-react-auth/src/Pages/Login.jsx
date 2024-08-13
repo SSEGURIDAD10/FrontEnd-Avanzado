584153124906a405ef104d153a35cdfd6ef429e8
Villagran <luisbmx_haro@hotmail.com>
2024-08-13 17:41:48 -0600

    Styles

--- 03-react-auth/src/Pages/Login.jsx
+++ 03-react-auth/src/Pages/Login.jsx
@@ -2,29 +2,36 @@ import "@/styles/form.css";
 
 const Login = () => {
   return (
-    <main className='form-singin w-100 m-auto'>
+    <main className='form-signin w-100 m-auto'>
       <div className="login">
-        <h1>Login</h1>
+        <h1 className='h3 mb-3 fw-normal'>Login</h1>
         <div className="login-container">
           <form>
-            <label htmlFor="simple-email">Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="correo@mail.com"
-              id="simple-email"
-              autoComplete="current-password"
-            />
+            <div className='form-floating'>
+              <input
+                className='form-control'
+                type="text"
+                name="email"
+                placeholder="correo@mail.com"
+                id="simple-email"
+                autoComplete="current-password"
+              />
+              <label htmlFor="floatingInput">Email adress</label>
+            </div>
 
-            <label htmlFor="simple-password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="simple-password"
-              autoComplete="current-password"
-            />
+            <div className='form-floating'>
+              <input
+                className='form-control'
+                type="password"
+                name="password"
+                id="simple-password"
+                autoComplete="current-password"
+                placeholder="Password"
+              />
+              <label htmlFor="simple-password">Password</label>
+            </div>
 
-            <button type="submit">Iniciar Sesion</button>
+            <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
           </form>
         </div>
       </div>
--- 03-react-auth/src/Pages/Signup.jsx
+++ 03-react-auth/src/Pages/Signup.jsx
@@ -2,62 +2,80 @@ import '@/styles/form.css'
 
 const Signup = () => {
   return (
-    <main className='form-singin w-100 m-auto'>
-      <div className="login">
-        <div className="login-container">
-          <form
-          >
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              name="firstName"
-              placeholder="Tu Nombre"
-              id="firstName"
-            />
+    <main className='form-signin w-100 m-auto'>
+      <form>
+        <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
 
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              placeholder="Tu Apellido"
-              id="lastName"
-            />
-
-            <label htmlFor="age">Age</label>
-            <input
-              type="number"
-              name="age"
-              placeholder="Tu Edad"
-              id="age"
-            />
-
-            <label htmlFor="gender">Gender</label>
-            <select name="gender" id="gender">
-              <option value="">Chose...</option>
-              <option value="M">Male</option>
-              <option value="F">Female</option>
-              <option value="O">Other</option>
-            </select>
-
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="correo@mail.com"
-              id="email"
-            />
-
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-            />
-
-            <button type="submit">Send It</button>
-          </form>
+        <div className='form-floating'>
+          <input
+            type='text'
+            className='form-control'
+            id='first_name'
+            name='first_name'
+            value=''
+            onChange={() => {}}
+            placeholder='John'
+          />
+          <label htmlFor='first_name'>First Name</label>
         </div>
-      </div>
+
+        <div className='form-floating'>
+          <input
+            type='text'
+            className='form-control'
+            id='last_name'
+            name='last_name'
+            value=''
+            onChange={() => {}}
+            placeholder='Doe'
+          />
+          <label htmlFor='last_name'>Last Name</label>
+        </div>
+
+        <div className='form-floating'>
+          <select
+            className='form-select'
+            id='gender'
+            name='gender'
+            value=''
+            onChange={() => {}}
+          >
+            <option value=''>Choose...</option>
+            <option value='M'>Male</option>
+            <option value='F'>Female</option>
+          </select>
+          <label htmlFor='gender'>Gender</label>
+        </div>
+
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            value=''
+            onChange={() => {}}
+            placeholder='name@example.com'
+          />
+          <label htmlFor='email'>Email address</label>
+        </div>
+
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            name='password'
+            value=''
+            onChange={() => {}}
+            placeholder='Password'
+          />
+          <label htmlFor='password'>Password</label>
+        </div>
+
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+      </form>
     </main>
   )
 }
