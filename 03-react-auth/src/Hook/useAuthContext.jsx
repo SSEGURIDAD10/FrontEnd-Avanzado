05260dbaf61582f1d69ecf457d385fee438cb03d
Villagran <luisbmx_haro@hotmail.com>
2024-08-17 20:11:46 -0600

    useAuthContext

--- 03-react-auth/src/Hook/useAuthContext.jsx
+++ 03-react-auth/src/Hook/useAuthContext.jsx
@@ -2,4 +2,12 @@
 import { useContext } from "react";
 import { AuthContext } from '@/Context/AuthContext';
 
-// 1.- Usar
+export const useAuthContext = () => {
+    // Quiero usar mi contexto y esto lo guardo en Context
+    const context = useContext(AuthContext)
+    // Si no se crea el contexto mando un error, si e undefined lanzo el error
+    if(!context) {
+        throw Error('useAuthContext debde ser usado dentro de AuthProvider')
+    }
+    return context
+}
