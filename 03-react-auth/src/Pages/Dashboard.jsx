607fb47e0ae0b0a2e34a78aa5f9cd9ff55d6dda3
Villagran <luisbmx_haro@hotmail.com>
2024-08-21 10:41:32 -0600

    Dashboard

--- 03-react-auth/src/Pages/Dashboard.jsx
+++ 03-react-auth/src/Pages/Dashboard.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { getUserServices } from '../Services/userServices'
 
 const Dashboard = () => {
+  const [userData, setUserData] = useState({})
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try{
+        const response = await getUserServices(token)
+        if(response.status === 200){
+          setUserData(response.data)
+        }
+      }catch(error){
+        console.log('Ocurrio un error en Dashboard', error)
+      }
+    }
+    fetchUserData()
+  },[token])
+
   return (
-    <div>Dashboard</div>
+    <>
+    {userData?.first_name && <p>{userData.first_name}</p>}
+    {userData?.last_name && <p>{userData.last_name}</p>}
+    {userData?.gender_name && <p>{userData.gender_name}</p>}
+    {userData?.email_name && <p>{userData.email_name}</p>}
+    {userData?.role_name && <p>{userData.role_name}</p>}
+    </>
   )
 }
 
